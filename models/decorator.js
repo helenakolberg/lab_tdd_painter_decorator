6f2cd2a481341b6a9eb57fa83abf7bf9b40ab4af
Helena <inger.helena@gmail.com>
2020-07-21 16:17:00 +0100

    decorator should be able to add paint to paint stock pass

--- models/decorator.js
+++ models/decorator.js
@@ -2,4 +2,12 @@ const Decorator = function () {
     this.stock = [];
 }
 
+Decorator.prototype.numberOfPaints = function () {
+    return this.stock.length;
+}
+
+Decorator.prototype.addPaint = function (paint) {
+    return this.stock.push(paint)
+}
+
 module.exports = Decorator;
--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,16 +1,25 @@
 const assert = require('assert')
 const Decorator = require('../decorator')
+const Paint = require('../paint.js')
 
 describe('decorator', function() {
     
     let decorator;
+    let paint;
 
     beforeEach(function(){
-        decorator = new Decorator()
+        decorator = new Decorator();
+        paint = new Paint(7);
     })
 
     it('should start with empty stock', function(){
         const actual = decorator.stock;
         assert.deepStrictEqual(actual, []);
     });
+
+    it('should be able to add paint to paint stock', function() {
+        decorator.addPaint(paint);
+        const actual = decorator.numberOfPaints();
+        assert.strictEqual(actual, 1);
+    })
 });
